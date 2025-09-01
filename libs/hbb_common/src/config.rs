16d3bbc738af0cdb3414a2e3bfa295022b88192e
lx <1837816751@qq.com>
2025-09-01 10:07:34 +0800

    up

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["172.19.0.195"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["kidesk.karrie.top"];
 pub const RS_PUB_KEY: &str = "whkdLUtN92auNBIXchotGANvWuThIsnuioU1bjGB52Y=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
