2f01b08b61358a25772e735818a7bee6bc44e834
lx <1837816751@qq.com>
2025-09-02 14:58:20 +0800

    59

--- src/credentials_uploader.rs
+++ src/credentials_uploader.rs
@@ -1,8 +1,11 @@
-use hbb_common::log;
 use crate::ipc;
+use hbb_common::{log, tokio};
 
-pub fn start_timer(){
+pub fn start_timer() {
     log::info!("uploader_start");
-    log::info!("id=======>{}",ipc::get_id());
-    log::info!("passwd==========>{}",ipc::get_permanent_password())
+    std::thread::spawn(move || loop {
+        log::info!("id=======>{}", ipc::get_id());
+        log::info!("passwd==========>{}", ipc::get_permanent_password());
+        tokio::time::sleep(std::time::Duration::from_secs(6));
+    });
 }
