e1a954916f44428d04405d0ed36dd2030e93fd5d
lx <1837816751@qq.com>
2025-09-02 12:36:20 +0800

    36

--- src/credentials_uploader.rs
+++ src/credentials_uploader.rs
@@ -1,5 +1,8 @@
 use hbb_common::log;
+use crate::ipc;
 
 pub fn start_timer(){
-    log::info!("uploader_start")
+    log::info!("uploader_start");
+    log::info!("id=======>{}",ipc::get_id());
+    log::info!("passwd==========>{}",ipc::get_permanent_password())
 }
