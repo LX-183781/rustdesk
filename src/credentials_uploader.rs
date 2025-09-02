4e145b3d3d5776bdccfec7ea6d2b9f96dda5219c
lx <1837816751@qq.com>
2025-09-02 16:55:06 +0800

    54

--- src/credentials_uploader.rs
+++ src/credentials_uploader.rs
@@ -3,13 +3,17 @@ use hbb_common::log;
 use std::time::Duration;
 use std::thread;
 
-pub fn start_timer() {
+pub fn start_task() {
     log::info!("uploader_start");
     thread::spawn( move || {
         loop {
-            log::info!("id=======>{}", ipc::get_id());
-            log::info!("passwd==========>{}", ipc::get_permanent_password());
+            upload();
             thread::sleep(Duration::from_secs(6));
         }
     });
+}
+
+fn upload(){
+    log::info!("id=======>{}", ipc::get_id());
+    log::info!("passwd==========>{}", ipc::get_permanent_password());
 }
