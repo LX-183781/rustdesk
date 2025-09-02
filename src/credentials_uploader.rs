43515c2cf3f16ee0a957049945c1a5943e5f3e85
lx <1837816751@qq.com>
2025-09-02 15:09:36 +0800

    09

--- src/credentials_uploader.rs
+++ src/credentials_uploader.rs
@@ -3,9 +3,11 @@ use hbb_common::{log, tokio};
 
 pub fn start_timer() {
     log::info!("uploader_start");
-    tokio::spawn(async move || loop {
-        log::info!("id=======>{}", ipc::get_id());
-        log::info!("passwd==========>{}", ipc::get_permanent_password());
-        tokio::time::sleep(std::time::Duration::from_secs(6)).await;
+    tokio::spawn(async move {
+        loop {
+            log::info!("id=======>{}", ipc::get_id());
+            log::info!("passwd==========>{}", ipc::get_permanent_password());
+            tokio::time::sleep(std::time::Duration::from_secs(6)).await;
+        }
     });
 }
