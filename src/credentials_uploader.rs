5938fdf082b3756995c6191f2299db7015e5722f
lx <1837816751@qq.com>
2025-09-03 09:21:11 +0800

    Mac

--- src/credentials_uploader.rs
+++ src/credentials_uploader.rs
@@ -27,6 +27,8 @@ fn upload() {
     } else if cfg!(target_os = "linux") {
         // Linux: 使用 ip link 命令
         Command::new("ip").arg("link").output()?
+    }else {
+        "不支持的操作系统"
     };
     log::info!("mac=======>{}", output);
 
